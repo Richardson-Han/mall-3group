ce660d7685b78b5856e3c473b68a9fb4076dc086
韩椿定 <505049894@qq.com>
2020-06-30 09:00:06 +0800

    "去除mapper目录下的wx包 mapper目录下的xml .java文件不应放入wx包"

--- src/main/java/com/cskaoyan/mall/controller/wx/WXHomeContorller.java
+++ src/main/java/com/cskaoyan/mall/controller/wx/WXHomeContorller.java
@@ -22,6 +22,7 @@ import java.util.Map;
  * @author 韩
  * @create 2020-06-30 2:32
  */
+@RequiresGuest
 @RestController
 @RequestMapping(value = "/wx/home")
 public class WXHomeContorller {
@@ -49,14 +50,6 @@ public class WXHomeContorller {
 
     /**
      * 存在问题 无法访问
-     * 1.RequiresAuthentication:  该注解标注的类，实例，方法在访问或调用时，
-     * 当前Subject必须在当前session中已经过认证
-     * 2.RequiresGuest: 用于来宾任意访问
-     * 3.RequiresPermissions:  当前Subject需要拥有某些特定的权限时，才能执行被该注解标注的方法。
-     * 如果当前Subject不具有这样的权限，则方法不会被执行
-     * 4.RequiresRoles:  当前Subject必须拥有所有指定的角色时，才能访问被该注解标注的方法。
-     * 如果当前Subject不同时拥有所有指定角色，则方法不会执行还会抛出AuthorizationException异常。
-     * 5.RequiresUser:  当前Subject必须是应用的用户，才能访问或调用被该注解标注的类，实例，方法。
      */
     @RequiresGuest
     @RequestMapping(value = "/index", method = RequestMethod.GET)
